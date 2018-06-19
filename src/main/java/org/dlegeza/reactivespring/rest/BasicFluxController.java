130ca9d8fd346e10428ca50b85702e13a84aa5e2
Dima Legeza <dmytro.legeza@powerhouse.nl>
2018-06-19 15:09:10 +0200

    [+] added basic flux service

--- src/main/java/org/dlegeza/reactivespring/rest/BasicFluxController.java
+++ src/main/java/org/dlegeza/reactivespring/rest/BasicFluxController.java
@@ -1,6 +1,7 @@
 package org.dlegeza.reactivespring.rest;
 
 import org.dlegeza.reactivespring.service.FluxService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ import reactor.core.publisher.Flux;
 public class BasicFluxController {
 	private final FluxService fluxService;
 
-	@GetMapping("{length}")
+	@GetMapping(value = "{length}", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
 	public Flux<Long> getNumbersWithTimeInterval(@PathVariable int length) {
 		return this.fluxService.getFlux(length);
 	}
